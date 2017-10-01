d202314c95b4daf93cbc4cd0fb0f9cac2e90b250
M W <mw32@users.noreply.github.com>
2017-10-01 10:21:22 -0700

    Added checks to problem selection.

--- math_quiz.py
+++ math_quiz.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python
+#
+# Copyright (c) 2016, CMW
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 import operator
 import random
@@ -16,8 +31,8 @@ OPS = {'1':operator.add,
        '3':operator.mul,
        '4':operator.div}
 
-type = max_int = max_time = None
-total = solved = error = 0
+type = max_int = max_time = "None"
+index = total = solved = error = 0
 
 
 def printplus(obj):
@@ -41,23 +56,37 @@ def get_type_descs():
     return d
 
 
+def reset_globals():
+    global type, max_int, max_time, index, solved, error, total
+    type = max_int = max_time = "None"
+    total = solved = error = 0
+    index = 1
+
+
 def get_input_params():
     global type, max_int, max_time
     type_prompt = "Select exercise type %s : " 
-    int_prompt = "\nEnter the max number to be used : "
+    int_prompt = "\nEnter the max round (integer) number to be used : "
     time_prompt = "\nEnter the max time (minutes) for the exercise: "
-    print "\nWelcome to Algebra Exercise!\n\nExercise types:"
-    printplus(get_type_descs())
-    type = raw_input(type_prompt % sorted(TYPES.keys()))
-    max_int = raw_input(int_prompt)
-    max_time = raw_input(time_prompt)
+    print "\nMathQuiz Algebra Exercise\n"
+    while type not in ['1', '2', '3', '4']:
+        print "\nExercise types:"
+        printplus(get_type_descs())
+        type = raw_input(type_prompt % sorted(TYPES.keys()))
+
+    while not max_int.isdigit():
+        max_int = raw_input(int_prompt)
+
+    while not max_time.isdigit():
+        max_time = raw_input(time_prompt)
+
     max_time = int(max_time) * 60
     #print "%s %s %s" % (type, max_int, max_time)
  
 
 def generate_problem():
     #print "%s %s %s" % (type, max_int, max_time)
-    global solved, error
+    global index, solved, error, total
     x = random.randint(0,int(max_int))
     y = random.randint(0,int(max_int))
 
@@ -69,19 +98,19 @@ def generate_problem():
         y = t
 
     ans = TYPES[type][1](x,y)
-    problem = "%s %s %s = " % (x, TYPES[type][2], y)
+    problem = "%2s) %s %s %s = " % (index, x, TYPES[type][2], y)
+    index += 1
     a = raw_input(problem)
-    # typo
     if not a.isdigit():
-        print "I/O Error: a number expected!\n"
-        return 
+        print "I/O Error! A number is expected.\n"
+        return
+    total += 1
     if ans == int(a):
         print "CORRECT!\n"
         solved += 1 
     else:
-        print "WRONG! Correct answer is: %s\n" % ans
+        print "WRONG! The correct answer is %s.\n" % ans
         error += 1
-    return
 
 
 def countdown(t):
@@ -91,8 +120,8 @@ def countdown(t):
 
 
 def timer_loop():
-    global max_time, total
-    raw_input ("\nHIT ENTER TO START!")
+    global max_time 
+    raw_input ("\n     HIT ENTER TO START!")
 
     #TODO: Thread
     #t = threading.Thread(target=countdown, args=max_time)
@@ -102,20 +131,36 @@ def timer_loop():
     start = time.time()
     while (time.time() - start <= int(max_time)):
         generate_problem()
-        total += 1
 
 
 def post_result():
     global total, solved, error
     print "\n\nTime is up!!!!"
-    print "\nTotal problems %s, correct: %s, wrong: %s.\n\n" %\
+    print "\nTotal problems attempted: %s, correct: %s, wrong: %s.\n\n" %\
         (total, solved, error)
 
 
 def run_exercise():
-    get_input_params()
-    timer_loop()
-    post_result()
+    start = True
+    while start:
+        a = None
+        reset_globals()
+        get_input_params()
+        timer_loop()
+        post_result()
+
+        while a not in ['y', 'Y', 'n', 'N', '']:
+            a = raw_input("Want to retry and improve your skills? [Y]/N: ")
+            if a in ['n', 'N']:
+                finish()
+                return
+            elif a in ['y', 'Y', '']:
+                start = True
+                
+
+def finish():
+    print "\nThank you for using MathQuiz!"
+    print "Copyright (c) 2016, CMW - GNU Public License\n\n"
 
 
 if __name__ == '__main__':
